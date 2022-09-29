5acd806992e838651460d53f78c438891df7c65a
yasinthon04 <yasinthon_r@cmu.ac.th>
2022-09-29 18:25:40 +0700

    1.6

--- src/main/java/se331/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/rest/dao/EventDaoDbImpl.java
@@ -37,7 +37,7 @@ public class EventDaoDbImpl implements EventDao {
 
     @Override
     public Page<Event> getEvent(String title, Pageable page) {
-        return eventRepository.findByTitleContaining(title, page);
+        return eventRepository.findByTitleContainingOrDescriptionContaining(title, title, page);
     }
 
 
--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -12,5 +12,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAll();
     Page<Event> findByTitle(String title, Pageable pageRequest);
-    Page<Event> findByTitleContaining(String title, Pageable pageable);
+    Page<Event> findByTitleContainingOrDescriptionContaining(String title, String description, Pageable pageRequest);
+
+    
 }
